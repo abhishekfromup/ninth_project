6be8e9e60c2dcf5389e06369eef9f7c68af071a4
Abhishek <abhishekfromup@gmail.com>
2019-08-04 16:54:41 +0530

    added StudentAdmin class in admin.py registered it, created superuser, made migrations

--- ninth_app/admin.py
+++ ninth_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student
 
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['student_id', 'student_name', 'student_email', 'student_date_of_birth', 'student_mobile']
 # Register your models here.
+
+admin.site.register(Student, StudentAdmin)
