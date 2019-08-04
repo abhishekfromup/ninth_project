66175f3e3b5defef5895c2a61b1abe650b47e85c
Abhishek <abhishekfromup@gmail.com>
2019-08-04 16:46:33 +0530

    added Student Model to models.py

--- ninth_app/models.py
+++ ninth_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+	student_id = models.AutoField(primary_key = True)
+	student_name = models.CharField(max_length = 30)
+	student_email = models.EmailField(max_length = 30)
+	student_date_of_birth = models.DateTimeField()
+	student_mobile = models.CharField(max_length = 12)
